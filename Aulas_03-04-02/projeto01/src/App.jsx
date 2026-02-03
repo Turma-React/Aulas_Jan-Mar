75ec8c597e49a1b18ff9955e2f7aafae70a9e937
Mauro Henrique Silva <silva.maurohenrique@gmail.com>
2026-02-03 20:12:30 +0000

    fix: Aulas 03_04/02

--- Aulas_03-04-02/projeto01/src/App.jsx
+++ Aulas_03-04-02/projeto01/src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TrabalheCososco from "./pages/TrabalheConosco";
 import Navbar from "./components/NavBar";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <>
       <h1>Estou em todas as páginas</h1>
-      <BrowserRouter>
-        <Navbar /> {/* ->  Precisa estar em BrowserRouter */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/vagas" element={<TrabalheCososco />} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar /> {/* ->  Precisa estar em BrowserRouter */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/vagas" element={<TrabalheCososco />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
--- Aulas_03-04-02/projeto01/src/components/NavBar.jsx
+++ Aulas_03-04-02/projeto01/src/components/NavBar.jsx
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 const NavBar = () => {
   return (
     <nav>
       {/* Não faz reload -> Troca de componentes */}
+      {/* NavLink é mais moderno */}
+      {/* 
       <Link to="/">Home</Link>
       <Link to="/vagas">Trabalhe conosco</Link>
+      */}
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/vagas">Trabalhe conosco</NavLink>
     </nav>
   );
 };
